75fb8d924f29a0d9eac5b71d6a1f414e01d611e9
Wingfooted <Wingfooted3@gmail.com>
2024-09-08 02:19:23 +1000

    Commenting on code

--- merge.py
+++ merge.py
@@ -4,11 +4,12 @@ markets = pd.read_csv("market_data.csv")
 temp = pd.read_csv("data_temp.csv")
 
 markets['date'] = pd.to_datetime(markets['date'])
-
 markets['Year'] = markets['date'].dt.year
-markets['Month'] = markets['date'].dt.strftime('%b')  
+markets['Month'] = markets['date'].dt.strftime('%b')
 markets_avg = markets.groupby(['Year', 'Month']).mean().reset_index()
 
-merged_df = pd.merge(temp, markets_avg, on=['Year', 'Month'])
+merged_df = pd.merge(temp, markets_avg, on=['Year', 'Month']) 
+# this drops all year-month pairings that do not exist within the dataframes
+# ie, if one dataframe is longer than the other, the total length is the determined by the shoretet DF
 
 merged_df.to_csv("integrated_data_set.csv")
